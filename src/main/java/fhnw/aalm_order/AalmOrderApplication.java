c4ea67ddedabf418f0b1c903f61f6b6c1b6fd56b
Teodor Glisic <teodor.glisic@students.fhnw.ch>
2025-12-09 21:27:43 +0100

    AB#54 Order Applikation erstellt

---
+++ src/main/java/fhnw/aalm_order/AalmOrderApplication.java
@@ -0,0 +1,13 @@
+package fhnw.aalm_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AalmOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AalmOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fhnw/aalm_order/AalmOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package fhnw.aalm_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AalmOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
